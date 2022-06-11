f612e0d1fb10040e2eff5466c8e9b1bd8634d1fc
Monique Berry <moberry098@gmail.com>
2022-06-11 12:14:26 -0500

    converted Item model class to Kotlin

--- app/src/main/java/com/example/fetchrewardsapplication/model/Item.kt
+++ app/src/main/java/com/example/fetchrewardsapplication/model/Item.kt
@@ -1,39 +1,18 @@
+package com.example.fetchrewardsapplication.model
 
-package com.example.fetchrewardsapplication.model;
+import com.google.gson.annotations.SerializedName
+import com.google.gson.annotations.Expose
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
-public class Item {
-
+class Item {
     @SerializedName("id")
     @Expose
-    private Integer id;
+    var id: Int? = null
+
     @SerializedName("listId")
     @Expose
-    private Integer listId;
+    var listId: Int? = null
+
     @SerializedName("name")
     @Expose
-    private String name;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getListId() {
-        return listId;
-    }
-
-    public void setListId(Integer listId) {
-        this.listId = listId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
+    val name: String? = null
 }
