22dfb7962d3b7e59b4e7bf3affd20f3b3a6d4675
Monique Berry <moberry098@gmail.com>
2022-06-11 12:39:48 -0500

    updated itemViewModel

--- app/src/main/java/com/example/fetchrewardsapplication/adapter/ItemsRVAdapter.kt
+++ app/src/main/java/com/example/fetchrewardsapplication/adapter/ItemsRVAdapter.kt
@@ -14,16 +14,14 @@ class ItemsRVAdapter(private val applicationContext: Context, private var items:
     class ItemsAdapterViewHolder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsAdapterViewHolder {
-        val view =
-            ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ItemsAdapterViewHolder(view)
+        return ItemsAdapterViewHolder(ItemLayoutBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false))
     }
     override fun onBindViewHolder(holder: ItemsAdapterViewHolder, position: Int) {
         with(holder) {
             with(items[position]) {
                 binding.id.text = applicationContext.getString(R.string.id, this.id.toString())
-                binding.listId.text =
-                    applicationContext.getString(R.string.listId, this.listId.toString())
+                binding.listId.text = applicationContext.getString(R.string.listId, this.listId.toString())
                 binding.name.text = applicationContext.getString(R.string.name, this.name)
             }
         }
